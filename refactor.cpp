08e33fac3109148afb5f03a85af451ac7c55c6b5
gregorioF <gregorio.freidin@mercadolibre.com>
2018-04-01 23:15:34 -0300

    change in structure

--- refactor.cpp
+++ refactor.cpp
@@ -8,34 +8,48 @@
 
 using namespace std;
 
-struct Rala{
-	int n;
-	vector<map<int, double> > conex; // lista de adyacencia. 
+class Rala{
+public:
+	Rala(int size ){
+		n = size;
 
-	vector<map<int,double> > transp; // transpuesta
+		vector< map<int, double>* > conecciones (n, NULL);
+		vector< map<int, double>* > transpuesta (n, NULL);
+
+		for(int i = 0 ; i < n ; i ++){
+			conecciones[i] = new map<int,double> ();
+			transpuesta[i] = new map<int,double> ();
+		}
+
+		conex = conecciones;
+		transp = transpuesta;
+	}
+
+	~Rala(){
+		for(int i = 0 ; i < n ; i ++){
+			delete(conex[i]);
+			delete(transp[i]);
+		}		
+	}
+
+	int n;
+	vector< map<int, double>* > conex; // lista de adyacencia. 
+
+	vector< map<int, double>* > transp; // transpuesta
 	// uso pair ya que necesitamos saber la columna a la que corresponde cada elemento para elimGauss
 	// pair.first = elemento, pair.second = columna
+
+
 };
 
-// Cabe aclarar que la struct puede cambiar, por eso me frené en la implementación hasta que definamos bien.
-// Sólo dejé acá un par de funciones boludas
 
-void crearRala(struct Rala* matriz, int n){
-	matriz->n = n;
-
-	vector<map<int, double> > conecciones (n, map<int, double>());
-
-	matriz->conex = conecciones;
-}
-
-
-void mostrarVectorPair(map<int,double>& m){
+void mostrarVectorPair(map<int,double>* m){
 	// cout << "longitud de vec: " << vec.size() << endl;
 	cout << "[";
-	for(map<int,double>::iterator it = m.begin(); it != m.end(); it++){
+	for(map<int,double>::iterator it = m->begin(); it != m->end(); it++){
 		map<int,double>::iterator next = it;
 		next ++; 
-		string comaOrEnd = next == m.end() ? "" : ", ";  
+		string comaOrEnd = next == m->end() ? "" : ", ";  
 		cout << "(" << it->first << "," << it -> second << ")" << comaOrEnd;
 	}
 	cout << "]" << endl;
@@ -48,8 +62,8 @@ void mostrarRala(Rala* matriz){
 }
 
 void insertarElemento(Rala* A, int fila, int columna, double valor ){
-	A -> conex[fila].insert(pair<int,double>(columna,valor));
-	A -> transp[columna].insert(pair<int,double>(fila,valor));
+	A -> conex[fila] -> insert(pair<int,double>(columna,valor));
+	A -> transp[columna] -> insert(pair<int,double>(fila,valor));
 }
 
 // devuelve el grado de la página j (o sea, la cantidad de elems en la columna j, o #linksSalientes)
@@ -58,7 +72,7 @@ int gradoSalida(struct Rala* A, int j){
 	int res = 0;
 	int n = A->n;
 	for(int i = 0; i < n; i++){
-		if( A->conex[i].find(j) != A->conex[i].end()){
+		if( A->conex[i] -> find(j) != A->conex[i] -> end()){
 			res++;
 		}
 	}
@@ -71,21 +85,21 @@ void sumaMatricial(Rala* A, Rala* B, Rala* C){
 	mostrarRala(A);
 	cout << "MATRIZ B: " << endl;
 	mostrarRala(B);
+	
 	int n = A->n;
-	crearRala(C, n);
 	for(int i = 0; i < n; i++){
-		map<int, double> filA = A->conex[i];
-		map<int, double> filB = B->conex[i];
+		map<int, double>* filA = A->conex[i];
+		map<int, double>* filB = B->conex[i];
 
-		map<int, double>::iterator itA = filA.begin();
-		map<int, double>::iterator itB = filB.begin();
+		map<int, double>::iterator itA = filA->begin();
+		map<int, double>::iterator itB = filB->begin();
 		
-		while(itA != filA.end() && itB != filB.end()){
-			if(itB == filB.end() ||  itA -> first < itB -> first){
+		while(itA != filA->end() && itB != filB->end()){
+			if(itB == filB->end() ||  itA -> first < itB -> first){
 				insertarElemento(C, i, itA->first, itA->second);
 				itA ++;
 			}
-			else if( itA == filA.end() || itA -> first > itB -> first){
+			else if( itA == filA->end() || itA -> first > itB -> first){
 				insertarElemento(C, i, itB->first, itB->second);
 				itB ++;
 			}
@@ -113,13 +127,12 @@ double multiplicarFilas(map<int,double>* filA, map<int,double>* colB, int n){
 // ojo que la dejé llena de couts, no estoy seguro si es el código ya que el buscarColumnaEnFila está fallando
 void multiplicacionMatricial(Rala* A, Rala* B, Rala* C){
 	int n = A->n;
-	crearRala(C, n);
 	for(int i = 0; i < n; i++){
 		for(int j = 0 ; j < n ; j ++){
-			map<int,double> filA = A->conex[i];
-			map<int,double> colB = B->transp[j];
+			map<int,double>* filA = A->conex[i];
+			map<int,double>* colB = B->transp[j];
 
-			double multRes = multiplicarFilas(&filA, &colB, n);
+			double multRes = multiplicarFilas(filA, colB, n);
 			if(multRes != 0 ){
 				insertarElemento(C,i,j,multRes);
 			}
@@ -128,21 +141,18 @@ void multiplicacionMatricial(Rala* A, Rala* B, Rala* C){
 }
 
 void multiplicacionPorEscalar(Rala* A, double valor ){
-	for (int i = 0; i < A->conex.size(); ++i){
-		for(map<int,double>::iterator it = (A -> conex[i]).begin(); it != (A -> conex[i]).end(); it++){
+	for (int i = 0; i < A->conex.size(); i++){
+		for(map<int,double>::iterator it = (A -> conex[i])->begin(); it != (A -> conex[i])->end(); it++){
 			it -> second *= valor;
 		}
 	}
 }
 
 
-
 void Test1ParaSuma(){ 	// pasa, todo OK
-	Rala A;
-	crearRala(&A, 3);
+	Rala A = Rala(3);
 
-	Rala B;
-	crearRala(&B, 3);
+	Rala B = Rala(3);
 
 	insertarElemento(&A, 0, 0, 4);
 	insertarElemento(&A, 0, 2, 2);
@@ -153,17 +163,15 @@ void Test1ParaSuma(){ 	// pasa, todo OK
 	insertarElemento(&B, 1, 1, 2);
 	insertarElemento(&B, 2, 2, 3);
 
-	Rala C;
+	Rala C = Rala(A.n);
 	sumaMatricial(&A, &B, &C);
 	mostrarRala(&C);
 }
 
 void Test1ParaMult(){
-	Rala A;
-	crearRala(&A, 3);
+	Rala A = Rala(3);
 
-	Rala B;
-	crearRala(&B, 3);
+	Rala B = Rala(3);
 
 	insertarElemento(&A, 0, 2, 3);
 	insertarElemento(&A, 1, 0, 2);
@@ -178,7 +186,7 @@ void Test1ParaMult(){
 	mostrarRala(&A);
 	mostrarRala(&B);
 
-	Rala C;
+	Rala C = Rala(A.n);
 	multiplicacionMatricial(&A, &B, &C);
 	cout << endl;
 	mostrarRala(&C);	
